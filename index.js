a77a8acb08bd44b743c959a5e4e0c3094be5f037
Barbara Dixon <babsnwosisi@yahoo.com>
2017-09-25 15:05:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,10 +12,8 @@ function yourAnimal() {
 
 function add2(n) {
   return n + two
-
-  // Feel free to move things around!
-  const two = 2
-}
+  console.log(n + 2)
+  }
 
 var funkyFunction = function() {
   return function() {
